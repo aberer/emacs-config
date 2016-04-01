24a5e02d9338095081487696a16ac2ee0ad17914
Andre Aberer <aaberer@bloomberg.net>
2016-04-01 18:52:30 +0200

    switch to rtags+company-mode
    * eradicates semantic + auto-complete

--- start-devel.el
+++ start-devel.el
@@ -15,7 +15,7 @@
 ;; SEMANTIC  ;;
 ;;;;;;;;;;;;;;;
 
-(semantic-mode 1)
+(semantic-mode 0)
 
 (global-semantic-idle-completions-mode t)
 (global-semantic-highlight-func-mode t)
@@ -150,7 +150,7 @@
 (defconst my-cc-style
   '("user" ;; c++-mode
     (c-offsets-alist . (
-                        (innamespace . 0)
+                        (innamespace . [0])
                         (substatement-open . 0)
                         ) ) )
   )
@@ -252,15 +252,15 @@
 ;; (c-toggle-hungry-state -1 )
 (setq speedbar-update-flag t)
 
-(defun my:ac-c-header-init()
-  (require 'auto-complete-c-headers)
-  (add-to-list 'ac-sources 'ac-source-c-headers)
-  (add-to-list 'ac-sources 'ac-source-semantic)
-  (add-to-list 'ac-sources 'ac-source-gtags)
-  )
+;; (defun my:ac-c-header-init()
+;;   (require 'auto-complete-c-headers)
+;;   (add-to-list 'ac-sources 'ac-source-c-headers)
+;;   (add-to-list 'ac-sources 'ac-source-semantic)
+;;   (add-to-list 'ac-sources 'ac-source-gtags)
+;;   )
 
-(add-hook 'c++-mode-hook 'my:ac-c-header-init)
-(add-hook 'c-mode-hook 'my:ac-c-header-init)
+;; (add-hook 'c++-mode-hook 'my:ac-c-header-init)
+;; (add-hook 'c-mode-hook 'my:ac-c-header-init)
 
 (setq puml-plantuml-jar-path "~/lib/plantuml.jar")
 
@@ -274,5 +274,19 @@
 (setq nxml-child-indent 4)
 (setq nxml-outline-child-indent 4)
 
+;; _____________________________________________________________________________
 
+;;;;;;;;;;;
+;; rtags ;;
+;;;;;;;;;;;
 
+;TODO: BEGIN cleanup 
+;; (load-file "/workspace/users/a.aberer/proj/rtags/src/rtags.el")
+(require 'rtags)
+(rtags-enable-standard-keybindings c-mode-base-map "\C-xr")
+
+(require 'company)
+(global-company-mode)
+(push 'company-rtags company-backends)
+(define-key c-mode-base-map (kbd "<C-tab>") (function company-complete))
+;TODO: END cleanup
