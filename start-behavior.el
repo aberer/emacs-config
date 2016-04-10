88498e6ffe055a95145710fc07f0068e0bba544f
Andre Aberer <andre.aberer@googlemail.com>
2016-04-10 18:34:48 +0200

    more helm actions

--- start-behavior.el
+++ start-behavior.el
@@ -98,4 +98,9 @@
       helm-ff-file-name-history-use-recentf t)
 
 (helm-mode 1)
+
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
