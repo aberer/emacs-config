9bb584b1620832df2bbeaef0d18d779d51834ff1
Andre Aberer <aaberer@bloomberg.net>
2016-07-04 08:43:11 -0400

    revises smart-parens

--- start-behavior.el
+++ start-behavior.el
@@ -121,13 +121,13 @@
 ;;;;;;;;;;;;;;;;;;
 ;; smart parens ;;
 ;;;;;;;;;;;;;;;;;;
-
 (use-package smartparens-config
   :ensure smartparens
-  :config (progn
-            (show-smartparens-global-mode t)
-            (add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
-            (add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode)))
+  :config
+  (setq sp-base-key-bindings (quote sp))
+  (smartparens-global-mode 1)
+  (smartparens-global-strict-mode 1)
+  (show-smartparens-global-mode 1))
 
 
 ;; _____________________________________________________________________________
